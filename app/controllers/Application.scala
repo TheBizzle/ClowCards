11ea01c99c5331839d6b259ee0e33f34a5f447a5
TheBizzle <jason.bertsche@gmail.com>
2013-06-09 01:20:17 -0400

    Allowing browsers to cache card images

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,13 @@ import
   play.api.mvc.{ Action, Controller }
 
 object Application extends Controller {
-  
+
+  def indexImage(path: String, file: String) = Action {
+    request =>
+      val cacheTime = 60 * 60 * 24 // Cache duration (seconds): 1 day
+      Assets.at(path, file)(request).withHeaders("Cache-Control" -> s"public, max-age=$cacheTime")
+  }
+
   def index = Action {
     Ok(views.html.index())
   }
