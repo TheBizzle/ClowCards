0a477f2c41f8e1a8c0db227ec48694bd3c36ad19
TheBizzle <jason.bertsche@gmail.com>
2013-07-19 00:16:36 -0500

    Moving cache time calculation to where it's relevant

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,11 +10,11 @@ object Application extends Controller {
 
   def indexImage(path: String, file: String) = Action {
     request =>
-      val CacheTime = Duration.standardDays(1).getStandardSeconds
       Assets.at(path, file)(request) match {
         case SimpleResult(ResponseHeader(NOT_FOUND, _), _) =>
           Assets.at("/public/images/index/priority", "question-mark.png")(request)
         case result =>
+          val CacheTime = Duration.standardDays(1).getStandardSeconds
           result.withHeaders("Cache-Control" -> s"public, max-age=$CacheTime")
       }
   }
